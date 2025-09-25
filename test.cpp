55a0a480a93408e373dc6d769936b6dd414a37f1
yycquant0812 <xherobruce@gmail.com>
2025-09-25 06:33:13 +0000

    作業3

--- test.cpp
+++ test.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 using namespace std;
-// 華視攝氏轉換
+
 int main() {
-    double fahrenheit;
-    double celsius;
-    cout << "請輸入華氏溫度:\n";
-    cin >> fahrenheit;
-    celsius = (fahrenheit - 32)/9*5;
-    cout << "攝氏溫度是:" << celsius;
+    
+    int score1, score2, score3;
+    cout << "請輸入3次分數(0~100): ";
+    cin >> score1 >> score2 >> score3;
+    double averageScore = (score1 + score2 + score3)/3;
+    if (score1 < 40 || score2 < 40 || score3 < 40) {
+        cout << "不及格\n";
+    } else if (averageScore >= 60) {
+        cout << "你及格\n";
+    } else if (averageScore < 60) {
+        cout << "不及格\n";
+    }
+    cout << "你的平均為: \a" << averageScore;
+
     return 0;
 }
+    
