a63a465b3c7f7f821a811906d225bf133a9013c8
yycquant0812 <xherobruce@gmail.com>
2025-10-02 13:56:48 +0800

    作業4

--- test1/main.cpp
+++ test1/main.cpp
@@ -1,7 +1,11 @@
- #include <iostream>
- int main() {
-   int x;            // 宣告（declaration）：跟編譯器說「我需要一個 int 變數叫 x」
-   x = 5;            // 指派（assignment）：把數值 5 放到 x 裡
-   std::cout << x;   // 這時輸出會是 5
-   return 0;
- }
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sum = 0;
+    for (int i = 1; i <= 50; i++) {
+        if ((i % 7) == 0) cout << i << " ";
+    }
+    return 0;
+}
+  
