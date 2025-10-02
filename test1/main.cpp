97fd919dce281f772618a25be80faec725f7709a
yycquant0812 <xherobruce@gmail.com>
2025-10-02 14:36:27 +0800

    hw4

--- test1/main.cpp
+++ test1/main.cpp
@@ -4,8 +4,11 @@ using namespace std;
 int main() {
     int sum = 0;
     for (int i = 1; i <= 50; i++) {
-        if ((i % 7) == 0) cout << i << " ";
+        if (i % 7 == 0) {
+            cout << i << " ";
+            sum += i;
+        }
     }
+    cout << "\n總和 = " << sum << endl;
     return 0;
 }
-  
